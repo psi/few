32f1a63d832273ba6c0cc70feca49a47cfd9b8c0
JD Harrington <psi@y0ru.net>
2010-05-04 00:40:11 -0400

    feed is now generated completely dynamically

--- feeds/vice_dos_and_donts.rb
+++ feeds/vice_dos_and_donts.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'cgi'
+
 class Few
   module Feeds
     class ViceDosAndDonts
@@ -14,14 +17,38 @@ class Few
       end
       
       def items
-        [
-          {
-            :title => "DON'T",
-            :description => "Shalom, Avi! Smart of you to get the sold-separately chinstrap and visor attachments for your yarmulke, because on the Gaza Strip they play Hacky Sack with rubble.",
-            :link => "http://www.viceland.com/int/dd.php?id=2394",
-            :timestamp => Time.parse("Apr 30, 2010").rfc822
-          }
-        ]
+        current_id = starting_id
+             
+        items = []
+        
+        10.times do
+          items << scrape_one(current_id)
+          current_id -= 1
+        end
+        
+        items
+      end
+      
+      def starting_id
+        @current_id ||= begin
+          body = open("http://www.viceland.com/int/dos.php").read
+          body.scan(%r|dos_donts/(\d+)/icon.jpg|).flatten.map(&:to_i).sort.last
+        end
+      end
+      
+      def scrape_one(id)
+        url  = "http://www.viceland.com/int/dd.php?id=#{id}"
+        body = open(url).read
+        
+        title     = body.match(/<h1>(.*?)<\/h1>/)[1]
+        caption   = body.scan(%r|<td\s+valign='top'\s+align='left'>([^>]+)<|mi).flatten.first.strip
+        image_url = "http://scs.viceland.com/img/dos_donts/#{id}/main.jpg"
+        
+        description = "&lt;img src='#{image_url}'&gt;&lt.br /&gt;&lt;p&gt;#{caption}&lt;/p&gt;"
+        
+        timestamp = Time.parse(body.match(/on ([^<]+) <i>wrote:/)[1]).rfc822 rescue Time.now.rfc822
+        
+        {:title => title, :link => url, :description => description, :timestamp => timestamp}
       end
     end
   end
